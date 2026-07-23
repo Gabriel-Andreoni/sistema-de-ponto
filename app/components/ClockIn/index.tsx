280d9b63f041d3804be6d9d8b3617477c9ce9ae5
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2026-07-22 22:48:53 -0300

    Criação da pasta fonts responsável por abrigar as fonts do projeto

--- app/components/ClockIn/index.tsx
+++ app/components/ClockIn/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {useState, useEffect} from 'react';
+import { blackOpsOne } from '@/app/fonts/fonts';
 
 export function ClockIn() {
     const [timer, setTimer] = useState<Date>();
@@ -17,9 +18,9 @@ export function ClockIn() {
 
     return (
         <section className="h-full bg-white/20 flex flex-col justify-center items-center gap-8 rounded-4xl ring-1 ring-white/80">
-            <span className="text-8xl">{timer?.getHours()}</span>
-            <span className="text-8xl">{timer?.getMinutes()}</span>
-            <span className="text-8xl">{timer?.getSeconds() != undefined && timer?.getSeconds() <= 9 ? "0" + timer?.getSeconds() : timer?.getSeconds()}</span>
+            <span className={`text-8xl font-bold ${blackOpsOne.className}`}>{timer?.getHours()}</span>
+            <span className={`text-8xl font-bold ${blackOpsOne.className}`}>{timer?.getMinutes()}</span>
+            <span className={`text-8xl font-bold ${blackOpsOne.className}`}>{timer?.getSeconds() != undefined && timer?.getSeconds() <= 9 ? "0" + timer?.getSeconds() : timer?.getSeconds()}</span>
         </section>
     )
 }
---
+++ app/fonts/fonts.ts
@@ -0,0 +1,11 @@
+import { Black_Ops_One, Oswald } from "next/font/google";
+
+export const blackOpsOne = Black_Ops_One({
+  subsets: ["latin"],
+  weight: ["400"]
+});
+
+export const oswald = Oswald({
+    subsets: ["latin"],
+    weight: ["400"]
+})
