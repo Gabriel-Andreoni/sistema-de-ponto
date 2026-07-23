1bd5a085ef9b756e08cc35909057948da8b671c1
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2026-07-22 22:31:28 -0300

    Criação do relógio e alterações na UI do componente

--- app/components/ClockIn/index.tsx
+++ app/components/ClockIn/index.tsx
@@ -1,7 +1,25 @@
-export function ClockIn() {
-    return (
-        <section className="h-full bg-green-500 rounded-4xl">
+"use client";
 
+import {useState, useEffect} from 'react';
+
+export function ClockIn() {
+    const [timer, setTimer] = useState<Date>();
+
+    useEffect(() => {
+        setTimer(new Date());
+
+        const timer = setInterval(() => {
+            setTimer(new Date())
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        <section className="h-full bg-white/20 flex flex-col justify-center items-center gap-8 rounded-4xl ring-1 ring-white/80">
+            <span className="text-8xl">{timer?.getHours()}</span>
+            <span className="text-8xl">{timer?.getMinutes()}</span>
+            <span className="text-8xl">{timer?.getSeconds() != undefined && timer?.getSeconds() <= 9 ? "0" + timer?.getSeconds() : timer?.getSeconds()}</span>
         </section>
     )
 }
--- app/components/UserInfo/index.tsx
+++ app/components/UserInfo/index.tsx
@@ -1,6 +1,6 @@
 export function UserInfo() {
     return (
-        <section className="h-6/12 col-span-1 bg-red-500 rounded-4xl">
+        <section className="h-6/12 col-span-1 bg-white/20 rounded-4xl ring-1 ring-white/80">
 
         </section>
     )
