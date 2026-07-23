8c77dc57c0b78c422775fd951d12dc71fc2d3d45
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2026-07-22 23:28:36 -0300

    Alterações na UI dos botões de ClockIn

--- app/components/ClockInButton/index.tsx
+++ app/components/ClockInButton/index.tsx
@@ -1,10 +1,30 @@
 "use client";
 
 export function ClockInButton() {
-    return (
-        <section className="p-8 col-start-2 col-end-5 flex justify-center items-center gap-8 bg-white/10  rounded-4xl ring-1 ring-white/80">
-            <button className="w-6/12 p-16 bg-[#0072f559] text-4xl font-bold rounded-4xl ring-1 ring-[#0f7bf7] cursor-pointer shadow-lg shadow-[#023877dc]">Bater Ponto</button>
-            <button className="w-6/12 p-16 bg-yellow-500/40 text-4xl font-bold rounded-4xl ring-1 ring-yellow-500/80 shadow-lg shadow-yellow-700/20 cursor-pointer">Editar Ponto</button>
-        </section>
-    )
+  return (
+    <section className="p-8 col-start-2 col-end-5 flex justify-center items-center gap-8 bg-white/10  rounded-4xl ring-1 ring-white/80">
+      <button className="w-6/12 p-16 relative overflow-hidden bg-zinc-900 text-white rounded-4xl">
+        <span
+          className="w-full h-full pointer-events-none absolute top-15 -left-145 rounded-full blur-3xl"
+          style={{ background: "#0554f2" }}
+        />
+        <span
+          className="w-full h-full pointer-events-none absolute -top-15 -right-145 rounded-full blur-3xl"
+          style={{ background: "#276bf3" }}
+        />
+        <span className="relative z-10 text-4xl cursor-pointer">Bater Ponto</span>
+      </button>
+       <button className="w-6/12 p-16 relative overflow-hidden bg-zinc-900 text-white rounded-4xl">
+        <span
+          className="w-full h-full pointer-events-none absolute top-15 -left-145 rounded-full blur-3xl"
+          style={{ background: "#0554f2" }}
+        />
+        <span
+          className="w-full h-full pointer-events-none absolute -top-15 -right-145 rounded-full blur-3xl"
+          style={{ background: "#276bf3" }}
+        />
+        <span className="relative z-10 text-4xl cursor-pointer">Editar Ponto</span>
+      </button>
+    </section>
+  );
 }
